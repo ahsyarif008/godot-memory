a7dd1ef36b06cb4b4b88dc46bfe72d5a8c105ae7
Ahmad Syarif Hidayat <bbmsyarif@gmail.com>
2025-02-15 15:14:55 +0700

    update cards

--- script/card.gd
+++ script/card.gd
@@ -1,7 +1,6 @@
 extends Node2D
 
 #card component
-@export var sprite_back: Texture2D
 @export var card_sprite: Sprite2D
 
 #card props
@@ -9,6 +8,8 @@ var card_id: int
 var card_name: String
 var is_flipped: bool
 var sprite_front: Texture2D
+@export var sprite_back: Texture2D
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -16,12 +17,18 @@ func _ready() -> void:
 	area.input_event.connect(_on_area_input_event)  # Connect the signal
 
 func _on_area_input_event(viewport, event, shape_idx):
-	if event is InputEventMouseButton and event.pressed:
+	if event is InputEventMouseButton and event.pressed and !is_flipped and get_parent().able_to_click_card:
 		reveal_card()
 
 func reveal_card():
 	is_flipped = true
+	var tween = create_tween()
+	tween.tween_property(self, "scale", Vector2(0, 1), 0.2)
+	await await get_tree().create_timer(0.2).timeout
+	tween = create_tween()
 	card_sprite.texture = sprite_front
+	tween.tween_property(self, "scale", Vector2(1, 1), 0.2)
+	get_parent().reveal_card_record(self)
 
 func conceal_card():
 	is_flipped = false
--- script/game_manager.gd
+++ script/game_manager.gd
@@ -1,9 +1,11 @@
 extends Node
 
 var card_data = [
-	{"id": 1, "name":"0 blue", "card_sprite": preload("res://assets/cards/blue/0_blue.png")},
-	{"id": 2, "name":"0 red", "card_sprite": preload("res://assets/cards/red/0_red.png")},
-	{"id": 3, "name":"0 green", "card_sprite": preload("res://assets/cards/green/0_green.png")}
+	{"id": 1, "name":"mango", "card_sprite": preload("res://assets/cards/card1.png")},
+	{"id": 2, "name":"pinneaple", "card_sprite": preload("res://assets/cards/card2.png")},
+	{"id": 3, "name":"cherry", "card_sprite": preload("res://assets/cards/card3.png")},
+	{"id": 4, "name":"coconut", "card_sprite": preload("res://assets/cards/card4.png")},
+	{"id": 5, "name":"watermelon", "card_sprite": preload("res://assets/cards/card5.png")}
 ]
 var card_deck = []
 
@@ -11,10 +13,14 @@ var card_deck = []
 @export var card_holder: Node2D
 
 #options
-var board_col:int = 3
+var board_col:int = 5
 var board_row:int = 2
 var board_col_start_pos:int = 100
 var board_row_start_pos:int = 150
+
+#states
+var card_revealed = []
+var able_to_click_card = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -33,14 +39,13 @@ func setup_boards():
 		for j in range(board_col):
 			places.append(Vector2(col,row))
 			col += 200
-		row += 300
+		row += 270
 	
 	card_deck.shuffle()
 
 	for i in range (card_deck.size()):
 		var card = card_scene.instantiate()
 		card.position = places[i]
-		print(places[i])
 		
 		#setup card property
 		card.card_id = card_deck[i].id
@@ -48,8 +53,27 @@ func setup_boards():
 		card.sprite_front = card_deck[i].card_sprite
 		
 		#attach to holder as parent
-		card_holder.add_child(card)
+		add_child(card)
 
+func reveal_card_record(card: Node2D):
+	card_revealed.append(card)
+	check_match()
+
+func check_match():
+	if card_revealed.size() >= 2:
+		able_to_click_card = false
+		if(card_revealed[0].card_id == card_revealed[1].card_id):
+			print("match")
+			await get_tree().create_timer(1.0).timeout
+			able_to_click_card = true
+		else:
+			print("not same, then reset")
+			able_to_click_card = false
+			await get_tree().create_timer(1.0).timeout
+			for card in card_revealed:
+				card.conceal_card()
+			able_to_click_card = true
+		card_revealed.clear()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
